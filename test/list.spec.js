731823c0b58ba415111b76f6c7f455385f00bfd0
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2016-09-04 22:18:12 +0200

    test: add chai expects

--- test/list.spec.js
+++ test/list.spec.js
@@ -1,21 +1,23 @@
-import expect from 'chai';
+/* eslint-disable no-unused-expressions */
+const expect = require('chai').expect;
+// import {expect} from 'chai';
 import {List} from '../lib/list';
 
 describe('List', () => {
   describe('constructor', () => {
     it('should initiate with no initial value', () => {
       const list = new List();
-      expect(list).toBeDefined();
-      expect(list.getLenght()).toBe(0);
+      expect(list).to.exist;
+      expect(list.getLength()).to.equal(0);
     });
 
     it('should initiate with an initial value', () => {
       const initialValue = 'initial';
       const list = new List(initialValue);
 
-      expect(list).toBeDefined();
-      expect(list.getLenght()).toBe(1);
-      expect(list.getHead().value).toBe(initialValue);
+      expect(list).to.exist;
+      expect(list.getLength()).to.equal(1);
+      expect(list.getHead().value).to.equal(initialValue);
     });
   });
 
@@ -25,8 +27,8 @@ describe('List', () => {
       const list = new List();
 
       values.forEach(value => list.pushBack(value));
-      expect(list.getLenght()).toBe(values.length);
-      expect(list.getTail().value).toBe(values[values.length - 1]);
+      expect(list.getLength()).to.equal(values.length);
+      expect(list.getTail().value).to.equal(values[values.length - 1]);
     });
   });
 
@@ -38,8 +40,8 @@ describe('List', () => {
       values.forEach(value => list.pushBack(value));
       list.push('inserted', 2);
 
-      expect(list.getLenght()).toBe(5);
-      expect(list.get(2).value).toEqual('inserted');
+      expect(list.getLength()).to.equal(5);
+      expect(list.get(2).value).to.equal('inserted');
     });
   });
 
@@ -56,23 +58,23 @@ describe('List', () => {
     it('should remove a specific node', () => {
       const valueToDelete = list.get(2).value;
       list.remove(valueToDelete);
-      expect(list.getLength()).toBe(3);
-      expect(list.find(valueToDelete)).toBeFalsy();
+      expect(list.getLength()).to.equal(3);
+      expect(list.find(valueToDelete)).to.equal.false;
     });
 
     it('should remove the head', () => {
       const previousHead = list.getHead();
       list.remove(previousHead.value);
-      expect(list.getLength()).toBe(3);
-      expect(list.getHead().value).toEqual(previousHead.next.value);
-      expect(list.find(previousHead.value)).toBeFalsy();
+      expect(list.getLength()).to.equal(3);
+      expect(list.getHead().value).to.equal(previousHead.next.value);
+      expect(list.find(previousHead.value)).to.not.exist;
     });
 
     it('should remove the tail', () => {
       const previousTail = list.getTail();
       list.remove(previousTail.value);
-      expect(list.getLength()).toBe(3);
-      expect(list.find(previousTail)).toBeFalsy();
+      expect(list.getLength()).to.equal(3);
+      expect(list.find(previousTail)).to.not.exist;
     });
   });
 
@@ -81,8 +83,8 @@ describe('List', () => {
       const list = new List();
       const values = list.getValues();
 
-      expect(Array.isArray(values)).toBe(true);
-      expect(values.length).toEqual(0);
+      expect(Array.isArray(values)).to.be.true;
+      expect(values.length).to.equal(0);
     });
 
     it('should return an array of values', () => {
@@ -91,9 +93,9 @@ describe('List', () => {
       initialValues.forEach(value => list.pushBack(value));
 
       const valuesGot = list.getValues();
-      expect(Array.isArray(valuesGot)).toBe(true);
-      expect(valuesGot.length).toBe(initialValues.length);
-      expect(valuesGot).toEqual(initialValues);
+      expect(Array.isArray(valuesGot)).to.be.true;
+      expect(valuesGot.length).to.equal(initialValues.length);
+      expect(valuesGot).to.deep.equal(initialValues);
     });
   });
 });
