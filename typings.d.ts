6dc2d555b7a10e7c7c5f1a3126cbf44f2711b6e8
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2016-09-10 11:03:16 +0200

    docs: change name in typings
    Previous name was misleading

--- typings.d.ts
+++ typings.d.ts
@@ -7,20 +7,20 @@ interface reduceCallback {
   (acc: Node | any, current: Node | any): any;
 }
 
-declare class jsList {
+declare class List {
   constructor(initialValue?: any);
   getLength(): number;
   getHead(): Node;
   getTail(): Node;
   reduce(callback: (acc: any, current: any) => any, startingValue: any, extractValues: boolean): any;
   forEach(callback: (value: any) => any);
-  map(callback: (value: any) => any): jsList;
+  map(callback: (value: any) => any): List;
   get(targetIndex: number): Node;
-  pushBack(newValue: any): jsList;
-  push(value: any, index: number): jsList;
-  remove(value: any): jsList;
+  pushBack(newValue: any): List;
+  push(value: any, index: number): List;
+  remove(value: any): List;
   find(value: any): Node;
   getValues(): any[];
 }
 
-export = jsList;
+export = List;
